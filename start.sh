bfc739fa5968a7da160ecd3005634a7d676c71cf
sumits <sumit.sur@outlook.com>
2025-06-20 10:09:48 +0530

    Refactor start.sh to clean up commented code and streamline API token retrieval

--- start.sh
+++ start.sh
@@ -7,25 +7,24 @@ echo "REPO ${REPO}"
 echo "ACCESS_TOKEN ${ACCESS_TOKEN}"
 
 
+# # 1. Check raw response & status
+# curl -i -H "Authorization: token ${ACCESS_TOKEN}" \
+#      -H "Accept: application/vnd.github+json" \
+#      https://api.github.com/repos/${REPO}/actions/runners/registration-token
 
-# 1. Check raw response & status
-curl -i -H "Authorization: token ${ACCESS_TOKEN}" \
-     -H "Accept: application/vnd.github+json" \
-     https://api.github.com/repos/${REPO}/actions/runners/registration-token
+# # 2. If that's good, inspect the JSON
+# TOKEN=$(curl -s -H "Authorization: token ${ACCESS_TOKEN}" \
+#                -H "Accept: application/vnd.github+json" \
+#                https://api.github.com/repos/${REPO}/actions/runners/registration-token)
 
-# 2. If that's good, inspect the JSON
-TOKEN=$(curl -s -H "Authorization: token ${ACCESS_TOKEN}" \
-               -H "Accept: application/vnd.github+json" \
-               https://api.github.com/repos/${REPO}/actions/runners/registration-token)
+# echo "Raw JSON: $TOKEN"
 
-echo "Raw JSON: $TOKEN"
+# echo "$TOKEN" | jq .token --raw-output
 
-echo "$TOKEN" | jq .token --raw-output
-
-REG_TOKEN = $(echo "$TOKEN" | jq .token --raw-output)
+# REG_TOKEN = $(echo "$TOKEN" | jq .token --raw-output)
 
 
-#REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
+REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
 
 
 echo "REG_TOKEN ${REG_TOKEN}"
